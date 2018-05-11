f2e51aa2f85bd47419a6cade750a026c4ff24b95
Luis Pérez <luis.perez@codeself.net>
2018-05-11 13:07:06 -0600

    feat: create Piece class and implement handleInput()

--- js/app.js
+++ js/app.js
@@ -1,24 +1,84 @@
-// Enemy our player must avoid
-class Enemy {
-    constructor() {
-        this.sprite = `images/enemy-bug.png`;
+/**
+ * Pieces class
+ * @description Due to Enemy and Player share update() and render() functions and both are 'pieces' of the game, this class will contain those 2 main functions.
+ */
+class Piece {
+    constructor(sprite) {
+        this.sprite = sprite;
     }
 
-    // Update the enemy's position, required method for game
+    // Update the piece's position, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
 
 	}
 	
-	// Draw the enemy on the screen, required method for game
+	// Draw the piece on the screen, required method for game
 	render() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	}
 }
 
+/**
+ * @description Enemy our player must avoid
+ */
+class Enemy extends Piece {
+    constructor() {
+        super(`images/enemy-bug.png`);
+    }
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+/**
+ * @description Our amazing player
+ */
+class Player extends Piece {
+    constructor() {
+        super(`images/char-boy.png`);
+    }
+
+    // Handle Input
+    handleInput(dir) {
+        switch (dir) {
+            case 'up':
+                this._moveUp();
+                break;
+            case 'down':
+                this._moveDown();
+                break;
+            case 'right':
+                this._moveRight();
+                break;
+            case 'left':
+                this._moveLeft();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Move up
+    _moveUp() {
+
+    }
+
+    // Move down
+    _moveDown() {
+
+    }
+
+    // Move right
+    _moveRight() {
+
+    }
+
+    // Move left
+    _moveLeft() {
+
+    }
+}
 
 
 // Now instantiate your objects.
